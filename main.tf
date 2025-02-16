a71ea1cbd92c07c99c6e2d193c58c507f86557ae
ptorresxcale <percy.t.salazar@xcaleconsulting.com>
2025-02-16 16:21:55 -0500

    security groups

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ module "networking" {
   cidr_private_subnet  = var.cidr_private_subnet
 }
 
-/*
+
 module "security_group" {
   source              = "./security-groups"
   ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
   vpc_id              = module.networking.dev_proj_1_vpc_id
   ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
 }
-
+/*
 module "jenkins" {
   source                    = "./jenkins"
   ami_id                    = var.ec2_ami_id
